0d2116815d18e3821313c6d467f243ff14b1e307
DaniloSts <dsb.danilo07@gmail.com>
2025-09-27 19:13:09 -0300

    Adicionado serviços da api

---
+++ services/patientsApi.mjs
@@ -0,0 +1,9 @@
+import { api } from "./api.mjs";
+
+export const patientsService = {
+  list: () => api.get("/rest/v1/patients"),
+  getById: (id) => api.get(`/rest/v1/patients/${id}`),
+  create: (data) => api.post("/rest/v1/patients", data),
+  update: (id, data) => api.patch(`/rest/v1/patients/${id}`, data),
+  delete: (id) => api.delete(`/rest/v1/patients/${id}`),
+};
