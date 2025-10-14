1538d37e512f86b28df68fd98f30509e00a10a7c
StsDanilo <dsb.danilo07@gmail.com>
2025-10-14 20:57:33 -0300

    adicionado horário semanal ao dashboard

--- app/doctor/dashboard/page.tsx
+++ app/doctor/dashboard/page.tsx
@@ -1,10 +1,91 @@
-import DoctorLayout from "@/components/doctor-layout"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import { Calendar, Clock, User, Plus } from "lucide-react"
-import Link from "next/link"
+"use client";
+
+import DoctorLayout from "@/components/doctor-layout";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Calendar, Clock, User, Plus } from "lucide-react";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { AvailabilityService } from "@/services/availabilityApi.mjs";
+
+type Availability = {
+    id: string;
+    doctor_id: string;
+    weekday: string;
+    start_time: string;
+    end_time: string;
+    slot_minutes: number;
+    appointment_type: string;
+    active: boolean;
+    created_at: string;
+    updated_at: string;
+    created_by: string;
+    updated_by: string | null;
+};
+
+type Schedule = {
+    weekday: object;
+};
 
 export default function PatientDashboard() {
+    const userInfo = JSON.parse(localStorage.getItem("user_info") || "{}");
+    const doctorId = "58ea5330-5cfe-4433-a218-2749844aee89"; //userInfo.id;
+    const [availability, setAvailability] = useState<any | null>(null);
+    const [schedule, setSchedule] = useState<Record<string, { start: string; end: string }[]>>({});
+    const formatTime = (time: string) => time.split(":").slice(0, 2).join(":");
+    // Mapa de tradução
+    const weekdaysPT: Record<string, string> = {
+        sunday: "Domingo",
+        monday: "Segunda",
+        tuesday: "Terça",
+        wednesday: "Quarta",
+        thursday: "Quinta",
+        friday: "Sexta",
+        saturday: "Sábado",
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await AvailabilityService.list();
+                const filteredResponse = response.filter((disp: { doctor_id: any }) => disp.doctor_id == doctorId);
+                setAvailability(filteredResponse);
+            } catch (e: any) {
+                alert(`${e?.error} ${e?.message}`);
+            }
+        };
+        fetchData();
+    }, []);
+
+    function formatAvailability(data: Availability[]) {
+        // Agrupar os horários por dia da semana
+        const schedule = data.reduce((acc: any, item) => {
+            const { weekday, start_time, end_time } = item;
+
+            // Se o dia ainda não existe, cria o array
+            if (!acc[weekday]) {
+                acc[weekday] = [];
+            }
+
+            // Adiciona o horário do dia
+            acc[weekday].push({
+                start: start_time,
+                end: end_time,
+            });
+
+            return acc;
+        }, {} as Record<string, { start: string; end: string }[]>);
+
+        return schedule;
+    }
+
+    useEffect(() => {
+        if (availability) {
+            const formatted = formatAvailability(availability);
+            setSchedule(formatted);
+        }
+    }, [availability]);
+
     return (
         <DoctorLayout>
             <div className="space-y-6">
@@ -85,7 +166,40 @@ export default function PatientDashboard() {
                         </CardContent>
                     </Card>
                 </div>
+                <div className="grid md:grid-cols-1 gap-6">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle>Horário Semanal</CardTitle>
+                            <CardDescription>Confira rapidamente a sua disponibilidade da semana</CardDescription>
+                        </CardHeader>
+                        <CardContent className="space-y-4 grid md:grid-cols-7 gap-2">
+                            {["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"].map((day) => {
+                                const times = schedule[day] || [];
+                                return (
+                                    <div key={day} className="space-y-4">
+                                        <div className="flex flex-col items-center justify-between p-3 bg-blue-50 rounded-lg">
+                                            <div>
+                                                <p className="font-medium capitalize">{weekdaysPT[day]}</p>
+                                            </div>
+                                            <div className="text-center">
+                                                {times.length > 0 ? (
+                                                    times.map((t, i) => (
+                                                        <p key={i} className="text-sm text-gray-600">
+                                                            {formatTime(t.start)} <br /> {formatTime(t.end)}
+                                                        </p>
+                                                    ))
+                                                ) : (
+                                                    <p className="text-sm text-gray-400 italic">Sem horário</p>
+                                                )}
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </DoctorLayout>
-    )
+    );
 }
--- app/doctor/disponibilidade/page.tsx
+++ app/doctor/disponibilidade/page.tsx
@@ -7,23 +7,77 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import DoctorLayout from "@/components/doctor-layout";
-import {AvailabilityService} from "@/services/availabilityApi.mjs"
+import { AvailabilityService } from "@/services/availabilityApi.mjs";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
 export default function AvailabilityPage() {
-   const [error, setError] = useState<string | null>(null);
-  
-    useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await AvailabilityService.list();
-      console.log(response);
-    } catch (e: any) {
-      alert(`${e?.error} ${e?.message}`);
-    }
-  };
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+    const userInfo = JSON.parse(localStorage.getItem("user_info") || "{}");
+    const [modalidadeConsulta, setModalidadeConsulta] = useState<string>("");
 
-  fetchData();
-}, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await AvailabilityService.list();
+                console.log(response);
+            } catch (e: any) {
+                alert(`${e?.error} ${e?.message}`);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (isLoading) return;
+        setIsLoading(true);
+        const form = e.currentTarget;
+        const formData = new FormData(form);
+
+        const apiPayload = {
+            doctor_id: userInfo.id,
+            created_by: userInfo.id,
+            weekday: (formData.get("weekday") as string) || undefined,
+            start_time: (formData.get("horarioEntrada") as string) || undefined,
+            end_time: (formData.get("horarioSaida") as string) || undefined,
+            slot_minutes: Number(formData.get("duracaoConsulta")) || undefined,
+            appointment_type: modalidadeConsulta || undefined,
+            active: true,
+        };
+        console.log(apiPayload);
+
+        try {
+            const res = await AvailabilityService.create(apiPayload);
+            console.log(res);
+
+            let message = "disponibilidade cadastrada com sucesso";
+            try {
+                if (!res[0].id) {
+                    throw new Error(`${res.error} ${res.message}` || "A API retornou erro");
+                } else {
+                    console.log(message);
+                }
+            } catch {}
+
+            toast({
+                title: "Sucesso",
+                description: message,
+            });
+            router.push("#"); // adicionar página para listar a disponibilidade
+        } catch (err: any) {
+            toast({
+                title: "Erro",
+                description: err?.message || "Não foi possível cadastrar o paciente",
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
         <DoctorLayout>
             <div className="space-y-6">
@@ -34,65 +88,65 @@ export default function AvailabilityPage() {
                     </div>
                 </div>
 
-                <form className="space-y-6">
+                <form className="space-y-6" onSubmit={handleSubmit}>
                     <div className="bg-white rounded-lg border border-gray-200 p-6">
                         <h2 className="text-lg font-semibold text-gray-900 mb-6">Dados </h2>
 
                         <div className="space-y-6">
-                    <div className="grid md:grid-cols-3 gap-4">
+                            <div className="grid md:grid-cols-3 gap-4">
                                 <div>
                                     <Label className="text-sm font-medium text-gray-700">Dia Da Semana</Label>
                                     <div className="flex gap-4 mt-2 flex-nowrap">
                                         <label className="flex items-center gap-2">
-                                            <input type="radio" name="sexo" value="masculino" className="text-blue-600" />
+                                            <input type="radio" name="weekday" value="monday" className="text-blue-600" />
                                             <span className="whitespace-nowrap text-sm">Segunda-Feira</span>
                                         </label>
                                         <label className="flex items-center gap-2">
-                                            <input type="radio" name="sexo" value="feminino" className="text-blue-600" />
+                                            <input type="radio" name="weekday" value="tuesday" className="text-blue-600" />
                                             <span className="whitespace-nowrap text-sm">Terça-Feira</span>
                                         </label>
                                         <label className="flex items-center gap-2">
-                                            <input type="radio" name="sexo" value="masculino" className="text-blue-600" />
+                                            <input type="radio" name="weekday" value="wednesday" className="text-blue-600" />
                                             <span className="whitespace-nowrap text-sm">Quarta-Feira</span>
                                         </label>
                                         <label className="flex items-center gap-2">
-                                            <input type="radio" name="sexo" value="masculino" className="text-blue-600" />
+                                            <input type="radio" name="weekday" value="thursday" className="text-blue-600" />
                                             <span className="whitespace-nowrap text-sm">Quinta-Feira</span>
                                         </label>
                                         <label className="flex items-center gap-2">
-                                            <input type="radio" name="sexo" value="masculino" className="text-blue-600" />
+                                            <input type="radio" name="weekday" value="friday" className="text-blue-600" />
                                             <span className="whitespace-nowrap text-sm">Sexta-Feira</span>
                                         </label>
                                         <label className="flex items-center gap-2">
-                                            <input type="radio" name="sexo" value="masculino" className="text-blue-600" />
+                                            <input type="radio" name="weekday" value="saturday" className="text-blue-600" />
                                             <span className="whitespace-nowrap text-sm">Sabado</span>
                                         </label>
                                         <label className="flex items-center gap-2">
-                                            <input type="radio" name="sexo" value="masculino" className="text-blue-600" />
+                                            <input type="radio" name="weekday" value="sunday" className="text-blue-600" />
                                             <span className="whitespace-nowrap text-sm">Domingo</span>
                                         </label>
                                     </div>
                                 </div>
                             </div>
-                            
-                            <div className="grid md:grid-cols-7 gap-6">
+
+                            <div className="grid md:grid-cols-5 gap-6">
                                 <div>
                                     <Label htmlFor="horarioEntrada" className="text-sm font-medium text-gray-700">
-                                        Horario De Entrada 
+                                        Horario De Entrada
                                     </Label>
-                                    <Input type="time" id="horarioEntrada"  required className="mt-1" />
+                                    <Input type="time" id="horarioEntrada" name="horarioEntrada" required className="mt-1" />
                                 </div>
                                 <div>
                                     <Label htmlFor="horarioSaida" className="text-sm font-medium text-gray-700">
                                         Horario De Saida
                                     </Label>
-                                    <Input type="time" id="horarioSaida"  required className="mt-1"  />
+                                    <Input type="time" id="horarioSaida" name="horarioSaida" required className="mt-1" />
                                 </div>
                                 <div>
                                     <Label htmlFor="duracaoConsulta" className="text-sm font-medium text-gray-700">
                                         Duração Da Consulta
                                     </Label>
-                                    <Input type="number" id="duracaoConsulta"  required className="mt-1" />
+                                    <Input type="number" id="duracaoConsulta" name="duracaoConsulta" required className="mt-1" />
                                 </div>
                             </div>
 
@@ -100,18 +154,16 @@ export default function AvailabilityPage() {
                                 <Label htmlFor="modalidadeConsulta" className="text-sm font-medium text-gray-700">
                                     Modalidade De Consulta
                                 </Label>
-                                <Select>
+                                <Select onValueChange={(value) => setModalidadeConsulta(value)} value={modalidadeConsulta}>
                                     <SelectTrigger className="mt-1">
                                         <SelectValue placeholder="Selecione" />
                                     </SelectTrigger>
                                     <SelectContent>
                                         <SelectItem value="presencial ">Presencial </SelectItem>
                                         <SelectItem value="telemedicina">Telemedicina</SelectItem>
-                                    
                                     </SelectContent>
                                 </Select>
                             </div>
-                            
                         </div>
                     </div>
 
@@ -120,7 +172,7 @@ export default function AvailabilityPage() {
                             <Button variant="outline">Cancelar</Button>
                         </Link>
                         <Button type="submit" className="bg-green-600 hover:bg-green-700">
-                            Salvar Disponibilidade 
+                            Salvar Disponibilidade
                         </Button>
                     </div>
                 </form>
