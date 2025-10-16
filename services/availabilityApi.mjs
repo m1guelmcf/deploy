6e215d5ae23fe2247189e6eeec5487e5232cbc78
StsDanilo <dsb.danilo07@gmail.com>
2025-10-16 09:04:03 -0300

    pequeno ajuste para api

--- services/availabilityApi.mjs
+++ services/availabilityApi.mjs
@@ -1,8 +1,9 @@
 import { api } from "./api.mjs";
 
 export const AvailabilityService = {
-  list: () => api.get("/rest/v1/doctor_availability"),
-  create: (data) => api.post("/rest/v1/doctor_availability", data),
-  update: (id, data) => api.patch(`/rest/v1/doctor_availability?id=eq.${id}`, data),
-  delete: (id) => api.delete(`/rest/v1/doctor_availability/{id}?id=eq.${id}`),
+    list: () => api.get("/rest/v1/doctor_availability"),
+    listById: (id) => api.get(`/rest/v1/doctor_availability?doctor_id=eq.${id}`),
+    create: (data) => api.post("/rest/v1/doctor_availability", data),
+    update: (id, data) => api.patch(`/rest/v1/doctor_availability?id=eq.${id}`, data),
+    delete: (id) => api.delete(`/rest/v1/doctor_availability?id=eq.${id}`),
 };
